c74961813ba4240266db562b3159d77af8fc0d12
Akani Manabe <akanimanabe@icloud.com>
2018-06-22 10:24:00 +0200

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Akani Manabe on 2018/06/22.
+//  Copyright © 2018 BBD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
